d1cffa98096ff88033c70e68045ffacd7552f0d6
Mahamodul Hasan Moon <coddermoon@gmail.com>
2023-01-29 03:30:42 +0600

    done task

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,15 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
-import { EmployeesModule } from './employees/employees.module';
-import { CatsService } from './modules/cats/cats.service';
+// import { EmployeesModule } from './employees/employees.module';
+// import { CatsService } from './modules/cats/cats.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGO_URI),
     UsersModule,
-    EmployeesModule,
   ],
-  providers: [CatsService],
+  providers: [],
 })
 export class AppModule {}
--- src/models/users.models.ts
+++ src/models/users.models.ts
@@ -16,6 +16,9 @@ export class User {
 
   @Prop({ required: true })
   country: string;
+
+  @Prop({ required: true })
+  state: string;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
